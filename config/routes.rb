b960672c2334048ef988cf8121e3104d1c1c7d52
lael aichaoui <lael.aichaoui@gmail.com>
2022-02-15 12:21:07 -0800

    create task method added and tested

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # List all tasks
+  get "/tasks", to: "tasks#index"
+
+  # Create a task
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+
+  # Read a task
+  get "/tasks/:id", to: "tasks#show", as: :task
 end
